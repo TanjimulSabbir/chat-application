bb18bb4ad6531cd8f1a7a8bef896e75e2a9328fa
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-12-29 15:25:59 +0600

    redux intital setup done:store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from '../features/Api/apiSlice';
+import authSlice from '../features/auth/authSlice';
+import conversationsSlice from '../features/conversations/conversationsSlice';
+import messagesSlice from '../features/messages/messagesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: "",
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSlice,
+    coversations: conversationsSlice,
+    messages: messagesSlice
   },
 });
