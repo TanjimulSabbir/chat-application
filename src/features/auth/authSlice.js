772e97d888b1c6126a735fae320b3d39bc44bf06
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-12-29 22:28:28 +0600

    working on registration boilerplate

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,16 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    auth: ""
+    accessToken: undefined,
+    user: undefined
 }
 
 const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-
+        userLoggedIn: (state, action) => {
+            state.accessToken = action.payload.accessToken;
+            state.user = action.payload.user;
+        },
+        userLoggedOut: (state, action) => {
+            state.accessToken = undefined;
+            state.user = undefined;
+        },
     }
 });
 
-export const {} = authSlice.actions;
+export const { userLoggedIn, userLoggedOut } = authSlice.actions;
 export default authSlice.reducer;
