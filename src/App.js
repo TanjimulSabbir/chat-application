4d94a44a929cd125cf82f0e3d0fbc5c256987971
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-12-30 18:02:11 +0600

    working on PrivateRoute

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
+import useAuthCheck from "./hooks/useAuthCheck";
 
 function App() {
+    const authChecked = useAuthCheck();
     return (
-        <Router>
+        authChecked ? <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
@@ -15,7 +17,7 @@ function App() {
                 <Route path="/inbox/:id" element={<Inbox />} />
             </Routes>
             <Toaster />
-        </Router>
+        </Router> : <div className="flex items-center justify-center text-green-500 text-2xl min-h-screen">User Authorization checking...</div>
     );
 }
 
---
+++ src/hooks/useAuthCheck.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { userLoggedIn } from "../features/auth/authSlice";
+
+function useAuthCheck() {
+    const [authChecked, setAuthChecked] = useState(false);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const auth = JSON.parse(localStorage?.getItem("auth"));
+        console.log(auth, "auth from localstorage in useAuthCheck")
+        if (auth?.accessToken && auth?.user) {
+            dispatch(userLoggedIn(auth))
+
+        }
+       setTimeout(() => {
+        setAuthChecked(true)
+       }, 3000);
+    }, [dispatch])
+
+    return authChecked;
+}
+
+export default useAuthCheck;
