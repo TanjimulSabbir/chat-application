c1ade665dc07959f3a0a983baf0852d6563bedb1
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-01 22:02:20 +0600

    modal messages all api created

--- src/features/users/usersApi.js
+++ src/features/users/usersApi.js
@@ -1,11 +1,22 @@
 import { apiSlice } from "../Api/apiSlice";
+import { useFindCoversationByEmailQuery } from "../conversations/conversationsApi";
 
 const usersApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         specifiedUser: builder.query({
             query: (email) => ({
                 url: `/users/${email}`
-            })
+            }),
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+                try {
+                    const result = await queryFulfilled;
+                    if (result.data.email) {
+                        dispatch()
+                    }
+                } catch (error) {
+
+                }
+            }
         })
     })
 })
