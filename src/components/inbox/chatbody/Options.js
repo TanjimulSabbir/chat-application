5092a6d266e86e329fbf2ab76ec44f450d605d15
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-03 23:43:00 +0600

    working on sending messages options:successfully message sent

--- src/components/inbox/chatbody/Options.js
+++ src/components/inbox/chatbody/Options.js
@@ -1,25 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useEditSpecifiedCoversationMutation } from "../../../features/conversations/conversationsApi";
+import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
 
-export default function Options(messages) {
+export default function Options({ messages }) {
     const [editSpecifiedCoversation, { isSuccess }] = useEditSpecifiedCoversationMutation();
-    console.log({ messages }, "Options");
-
-
+    const { user: loggedInUser } = useSelector(state => state.auth) || {};
     const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        if (isSuccess) {
+            toast.success("Message Sent!")
+        }
+        setMessage("")
+    }, [isSuccess])
+
+
+    const receiver = messages[0]?.receiver?.email === loggedInUser.email ? messages[0]?.sender : messages[0]?.receiver;
+
+
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        // editSpecifiedCoversation({
-        //     id: searchedConversations[0].id,
-        //     sender: loggedInUser, receiver: user[0],
-        //     data: {
-        //         participants: `${loggedInUser.email}-${messageData.email}`,
-        //         users: [{ ...loggedInUser }, { ...user[0] }],
-        //         message: messageData.text,
-        //         timestamp: new Date().getTime()
-        //     }
+        editSpecifiedCoversation({
+            id: messages[0]?.conversationId,
+            sender: loggedInUser,
+            receiver: receiver,
+            data: {
+                participants: `${loggedInUser?.email}-${receiver?.email}`,
+                users: [{ ...loggedInUser }, { ...receiver }],
+                message,
+                timestamp: new Date().getTime()
+            }
 
-        // })
+        })
+        console.log({
+            id: messages[0]?.conversationId,
+            sender: loggedInUser,
+            receiver: receiver,
+            data: {
+                participants: `${loggedInUser?.email}-${receiver?.email}`,
+                users: [{ ...loggedInUser }, { ...receiver }],
+                message,
+                timestamp: new Date().getTime()
+            }
+        }, "from options sending properties");
+
     }
     return (
         <form className="flex items-center justify-between w-full p-3 border-t border-gray-300"
@@ -27,7 +53,7 @@ export default function Options(messages) {
             <input
                 type="text"
                 placeholder="Message"
-                className="block w-full py-2 pl-4 mx-3 bg-gray-100 focus:ring focus:ring-violet-500 rounded-full outline-none focus:text-gray-700"
+                className="block w-full py-2 pl-4 mx-3 bg-gray-300 focus:ring focus:ring-violet-500 rounded-full outline-none focus:text-black"
                 name="message"
                 value={message}
                 onChange={(event) => setMessage(event.target.value)}
@@ -35,7 +61,7 @@ export default function Options(messages) {
             />
             <button type="submit">
                 <svg
-                    className="w-5 h-5 text-gray-500 origin-center transform rotate-90"
+                    className="w-6 h-6 text-green-600 origin-center transform rotate-90"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
