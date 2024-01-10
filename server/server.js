69da3ad5cc2d3d45b1ea428d03721fb97d6dd58a
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-10 22:36:09 +0600

    server side socket written

--- server/server.js
+++ server/server.js
@@ -10,9 +10,13 @@ const router = jsonServer.router("db.json");
 global.io = io;
 
 router.render = (req, res) => {
-    const path=req.path;
-    const method=req.method;
-    
+    const path = req.path;
+    const method = req.method;
+    if (path.includes("/conversations") && (method === "POST" || method === "PATCh")){
+        io.emit("conversation",{
+            data:res.locals.data
+        })
+    }
     console.log(req.locals?.data)
     res.json(res.locals?.data)
 }
